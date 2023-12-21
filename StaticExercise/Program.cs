cca3d25ee0f282ad8f71a27c02391e8d86ea1d63
Hhertilien1 <kinghertilien@gmail.com>
2023-12-21 14:48:27 -0500

    Completed Excersize part 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is the temperature in degrees fahrenheit?");
+            double fahrenheitValue = double.Parse(Console.ReadLine());
+            double celsiusValue = TempConverter.FahrenheitToCelsius(fahrenheitValue);
+            Console.WriteLine($"{fahrenheitValue} Fahrenheit is {celsiusValue} Celsius.");
+
+
+            Console.WriteLine("What is the temperature in degrees celsius)?");
+            double celsiusValue2 = double.Parse(Console.ReadLine());
+            double fahrenheitValue2 = TempConverter.CelsiusToFahrenheit(celsiusValue2);
+            Console.WriteLine($"{celsiusValue2} Celsius is {fahrenheitValue2} Fahrenheit.");
         }
     }
 }
